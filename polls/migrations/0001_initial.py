5aab75512688df9fe92db203df926f4cc41a531e
babydog090 <babydog090@gmail.com>
2023-08-16 00:00:00 +0900

    "2023-08-16 modified"

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -3,16 +3,11 @@
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
+import django.utils.timezone
 
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-    ]
 
-    operations = [
-        migrations.CreateModel(
-            name='Choice',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+class Migration(migrations.Migration):
+
                 ('choice_text', models.CharField(max_length=255)),
             ],
         ),
