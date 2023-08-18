5b2c7dc3ad143aaaa09bd73346b2547d6d56a2e9
babydog090 <babydog090@gmail.com>
2023-08-19 00:00:00 +0900

    "2023-08-19 modified"

--- polls/forms.py
+++ polls/forms.py
@@ -23,13 +23,3 @@ class EditPollForm(forms.ModelForm):
         fields = ['text', ]
         widgets = {
             'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'cols': 20}),
-        }
-
-
-class ChoiceAddForm(forms.ModelForm):
-    class Meta:
-        model = Choice
-        fields = ['choice_text', ]
-        widgets = {
-            'choice_text': forms.TextInput(attrs={'class': 'form-control', })
-        }
--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -18,11 +18,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Choice',
             fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('choice_text', models.CharField(max_length=255)),
             ],
         ),
-        migrations.AddField(
-            model_name='choice',
-            name='poll',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.poll'),
-        ),
-    ]
+        migrations.CreateModel(
