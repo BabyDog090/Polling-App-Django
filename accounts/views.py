001c53d6bbfb2ac4c7676e77ae113d28c68f83fa
babydog090 <babydog090@gmail.com>
2023-11-08 04:40:14 +0900

    "modified on 2023-11-08"

--- accounts/views.py
+++ accounts/views.py
@@ -8,17 +8,6 @@ from django.http import HttpResponse
 
 def login_user(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            redirect_url = request.GET.get('next', 'home')
-            return redirect(redirect_url)
-        else:
-            messages.error(request, "Username Or Password is incorrect!",
-                           extra_tags='alert alert-warning alert-dismissible fade show')
 
     return render(request, 'accounts/login.html')
 
